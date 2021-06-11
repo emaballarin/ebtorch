c5ceec8b4f3f2a017158c6afafef5ac7b24cbeb1
Emanuele Ballarin <emanuele@ballarin.cc>
2021-06-11 12:54:28 +0200

    ✨ Add Causal Convolution Layer 1D

--- ebtorch/nn/architectures.py
+++ ebtorch/nn/architectures.py
@@ -83,3 +83,39 @@ class FCBlock(nn.Module):
         if self.oactiv is not None:
             x: Tensor = self.oactiv(x)
         return x
+
+
+# Causal Convolutional Layer, 1D
+# (cfr.: https://github.com/pytorch/pytorch/issues/1333#issuecomment-400338207)
+# TODO: possibly tidy-up the code!
+
+
+class CausalConv1d(nn.Conv1d):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        dilation=1,
+        groups=1,
+        bias=True,
+    ):
+        self.__padding = (kernel_size - 1) * dilation
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=self.__padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+        )
+
+    def forward(self, x):
+        result = super(CausalConv1d, self).forward(x)
+        if self.__padding != 0:
+            return result[:, :, : -self.__padding]
+        return result
