d52f22538a1c9e52652671c1f05f49e0fc879ee0
Emanuele Ballarin <emanuele@ballarin.cc>
2023-03-08 18:24:08 +0100

    Update SAM optimizer: now (Adaptive) SAM, with optional Surrogate Gap Minimization
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- ebtorch/optim/sam.py
+++ ebtorch/optim/sam.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 # ==============================================================================
 #
-# Copyright (c) 2020-* David Samuel. All Rights Reserved. MIT Licensed.
-#                      [orig. code: https://github.com/davda54/sam ;
-#                       license text: https://github.com/davda54/sam/blob/master/LICENSE]
+# Copyright (c) 2020-2022 David Samuel. All Rights Reserved. MIT Licensed.
+#                         [orig. code: https://github.com/davda54/sam ;
+#                          license text: https://github.com/davda54/sam/blob/master/LICENSE]
 #
 # ==============================================================================
 #
-# Copyright (c) 2020-* Pierre Foret, Ariel Kleiner, Hossein Mobahi,
-#                      Behnam Neyshabur. All Rights Reserved. Apache 2.0 licensed.
-#                      [orig. work: https://arxiv.org/pdf/2010.01412.pdf;
-#                       orig. code: https://github.com/google-research/sam;
-#                       license text: https://github.com/google-research/sam/blob/main/LICENSE]
+# Copyright (c) 2020 Pierre Foret, Ariel Kleiner, Hossein Mobahi,
+#                    Behnam Neyshabur. All Rights Reserved. Apache 2.0 licensed.
+#                    [orig. work: https://arxiv.org/pdf/2010.01412.pdf;
+#                     orig. code: https://github.com/google-research/sam;
+#                     license text: https://github.com/google-research/sam/blob/main/LICENSE]
 #
 # ==============================================================================
 #
@@ -27,14 +27,21 @@ import torch
 
 
 class SAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+    def __init__(
+        self, params, base_optimizer, rho=0.05, alpha=0.0, adaptive=False, **kwargs
+    ):
         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+        assert alpha >= 0.0, f"Invalid alpha, should be non-negative: {alpha}"
 
-        defaults = dict(rho=rho, **kwargs)
+        defaults = dict(rho=rho, alpha=alpha, adaptive=adaptive, **kwargs)
         super(SAM, self).__init__(params, defaults)
 
         self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
         self.param_groups = self.base_optimizer.param_groups
+        self.defaults.update(self.base_optimizer.defaults)
+        self.minimize_surrogate_gap = any(
+            group["alpha"] > 0.0 for group in self.param_groups
+        )
 
     @torch.no_grad()
     def first_step(self, zero_grad=False):
@@ -45,9 +52,15 @@ class SAM(torch.optim.Optimizer):
             for p in group["params"]:
                 if p.grad is None:
                     continue
-                e_w = p.grad * scale.to(p)
+                self.state[p]["old_p"] = p.data.clone()
+                if self.minimize_surrogate_gap:
+                    self.state[p]["old_g"] = p.grad.data.clone()
+                e_w = (
+                    (torch.pow(p, 2) if group["adaptive"] else 1.0)
+                    * p.grad
+                    * scale.to(p)
+                )
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
-                self.state[p]["e_w"] = e_w
 
         if zero_grad:
             self.zero_grad()
@@ -58,7 +71,10 @@ class SAM(torch.optim.Optimizer):
             for p in group["params"]:
                 if p.grad is None:
                     continue
-                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
+
+        if self.minimize_surrogate_gap:
+            self._gradient_decompose()
 
         self.base_optimizer.step()  # do the actual "sharpness-aware" update
 
@@ -78,6 +94,26 @@ class SAM(torch.optim.Optimizer):
         closure()
         self.second_step()
 
+    def _gradient_decompose(self):
+        coeff_nomin, coeff_denom = 0.0, 0.0
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                coeff_nomin += (self.state[p]["old_g"] * p.grad).sum()
+                coeff_denom += p.grad.pow(2).sum()
+
+        coeff = coeff_nomin / (coeff_denom + 1e-12)
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                rejection = self.state[p]["old_g"] - coeff * p.grad
+                p.grad.data.add_(rejection, alpha=-group["alpha"])
+
     def _grad_norm(self):
         shared_device = self.param_groups[0]["params"][
             0
@@ -85,7 +121,9 @@ class SAM(torch.optim.Optimizer):
         norm = torch.norm(
             torch.stack(
                 [
-                    p.grad.norm(p=2).to(shared_device)
+                    ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad)
+                    .norm(p=2)
+                    .to(shared_device)
                     for group in self.param_groups
                     for p in group["params"]
                     if p.grad is not None
@@ -94,3 +132,7 @@ class SAM(torch.optim.Optimizer):
             p=2,
         )
         return norm
+
+    def load_state_dict(self, state_dict):
+        super().load_state_dict(state_dict)
+        self.base_optimizer.param_groups = self.param_groups
