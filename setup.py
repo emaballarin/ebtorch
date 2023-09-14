3b9ee378ad91d9bc7866fd7345328f98f2473f73
Emanuele Ballarin <emanuele@ballarin.cc>
2023-09-15 01:06:27 +0200

    Just version bump!
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ PACKAGENAME: str = "ebtorch"
 
 setup(
     name=PACKAGENAME,
-    version="0.13.2",
+    version="0.14.0",
     author="Emanuele Ballarin",
     author_email="emanuele@ballarin.cc",
     url="https://github.com/emaballarin/ebtorch",
